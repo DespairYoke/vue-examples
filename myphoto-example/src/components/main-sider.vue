f3f4ed03e753e146274e0ff7314b69cb89c2b895
zwd-admin <810095178@qq.com>
2018-10-22 20:32:31 +0800

    zw

---
+++ myphoto-example/src/components/main-sider.vue
@@ -0,0 +1,105 @@
+<template>
+<div>
+    <div style="height: 85px">
+        <img src="../assets/logo.png" style="height: 85px"/>
+    </div>
+    <Menu active-name="1-2" theme="dark" width="auto" >
+        <Submenu name="1">
+            <template slot="title">
+                <Icon type="ios-navigate"></Icon>
+                    Item 1
+            </template>
+            <MenuItem name="1-1">
+                <span @click="uploadimg">Option 1</span>
+            </MenuItem>
+            <MenuItem name="1-2">
+                
+                <span>Option 2</span>
+            </MenuItem>
+            <MenuItem name="1-3">
+                <span>Option 3</span>
+            </MenuItem>
+        </Submenu>
+        <Submenu name="2">
+            <template slot="title">
+                <Icon type="ios-keypad"></Icon>
+                    Item 2
+            </template>
+            <MenuItem name="1-1">
+                <span>Option 1</span>
+            </MenuItem>
+            <MenuItem name="1-2">
+                <span>Option 2</span>
+            </MenuItem>
+            <MenuItem name="1-3">
+                <span>Option 3</span>
+            </MenuItem>
+        </Submenu>
+        <Submenu name="3">
+            <template slot="title">
+                <Icon type="ios-analytics"></Icon>
+                    Item 3
+            </template>
+            <MenuItem name="1-1">
+               
+                <span>Option 1</span>
+            </MenuItem>
+            <MenuItem name="1-2">
+        
+                <span>Option 2</span>
+            </MenuItem>
+            <MenuItem name="1-3">
+               
+                <span>Option 3</span>
+            </MenuItem>
+        </Submenu>
+    </Menu>
+</div>
+</template>
+<script>
+export default {
+
+    methods: {
+        uploadimg() {
+            console.log("zzz")
+        }
+    }
+}
+</script>
+<style>
+.menu-item span{
+    display: inline-block;
+    overflow: hidden;
+    width: 69px;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    vertical-align: bottom;
+    transition: width .2s ease .2s;
+}
+.menu-item i{
+    transform: translateX(0px);
+    transition: font-size .2s ease, transform .2s ease;
+    vertical-align: middle;
+    font-size: 16px;
+}
+.collapsed-menu span{
+    width: 0px;
+    transition: width .2s ease;
+}
+.collapsed-menu i{
+    transform: translateX(5px);
+    transition: font-size .2s ease .2s, transform .2s ease .2s;
+    vertical-align: middle;
+    font-size: 22px;
+}
+.ivu-layout-sider-children {
+    background: #273039
+}
+.ivu-menu-dark {
+     background: #273039 !important;
+}
+.layout-header-bar.ivu-layout-header {
+    text-align: left;
+}
+</style>
+
---
+++ myphoto-example/src/page/main.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="layout">
+        <Layout>
+            <Sider ref="side1" hide-trigger collapsible :collapsed-width="78" v-model="isCollapsed">
+                <MenuSider></MenuSider>
+            </Sider>
+            <Layout>
+                <Header :style="{padding: 0}" class="layout-header-bar">
+                    <Icon @click.native="collapsedSider" :class="rotateIcon" :style="{margin: '0 20px'}" type="md-menu" size="24"></Icon>
+                    <span >首页</span>
+                </Header>
+                <Content :style="{margin: '20px', background: '#fff', minHeight: '260px'}">
+                    <Upload style="margin-top:50px"></Upload>
+                </Content>
+            </Layout>
+        </Layout>
+    </div>
+</template>
+<script>
+import MenuSider from '../components/main-sider.vue'
+import Upload from '../components/Upload.vue'
+    export default {
+        data () {
+            return {
+                isCollapsed: false
+            }
+        },
+        computed: {
+            rotateIcon () {
+                return [
+                    'menu-icon',
+                    this.isCollapsed ? 'rotate-icon' : ''
+                ];
+            }
+        },
+        methods: {
+            collapsedSider () {
+                this.$refs.side1.toggleCollapse();
+            }
+        },
+        components: {
+            MenuSider,
+            Upload
+        }
+    }
+</script>
+<style scoped>
+    .layout{
+        border: 1px solid #d7dde4;
+        background: #f5f7f9;
+        position: absolute;
+        border-radius: 4px;
+        overflow: hidden;
+        bottom: 0;
+        top: 0;
+        left: 0;
+        right: 0;
+       
+    }
+    .ivu-layout.ivu-layout-has-sider {
+        width: 100%;
+        height: 100%;
+        /* position: absolute;
+        bottom: 0;
+        top: 0;
+        left: ; */
+    }
+    .layout-header-bar{
+        background: #fff;
+        box-shadow: 0 1px 1px rgba(0,0,0,.1);
+    }
+    .layout-logo-left{
+        width: 90%;
+        height: 30px;
+        background: #5b6270;
+        border-radius: 3px;
+        margin: 15px auto;
+    }
+    .menu-icon{
+        transition: all .3s;
+    }
+    .rotate-icon{
+        transform: rotate(-90deg);
+    }
+
+</style>
