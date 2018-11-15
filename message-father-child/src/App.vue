ac121b98ff349b0be732e48b1a105477144f2cee
zwd-admin <810095178@qq.com>
2018-11-15 20:08:38 +0800

    zwd

---
+++ message-father-child/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+　　<div>
+　　　　<p> father</p>
+   　　 <hello :mes="loginJson.animal"  @sendiptVal='showChildMsg'></hello>
+        <p>{{this.message}}</p>
+　　</div>
+
+</template>
+
+<script>
+import hello from '@/components/hello'
+    
+export default {
+    name: 'app',
+  data() {
+    return {
+
+      message:'',
+      loginJson:{
+          "animal":"dog"
+      },
+
+    }
+  },
+  components:{
+    hello
+      
+  },
+  methods: {
+      showChildMsg(e) {
+          console.log(e)
+        this.message = e
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
